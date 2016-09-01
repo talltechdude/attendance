9d9531c2ce9524768ce2edd51652fb344291add3
Scott Linton <talltechdude@gmail.com>
2016-09-01 20:25:44 +1000

    Add group leaders

--- app/lib/database.js
+++ app/lib/database.js
@@ -94,7 +94,7 @@ exports.database = function database(_CCB) {
                 name:CCB.node_text('name', node),
                 description: CCB.node_text('description', node),
                 department:{id:CCB.node_attribute('department', 'id', node), name:CCB.node_text('department', node)},
-                leader:{id:CCB.node_attribute('main_leader', 'id', node), name:CCB.node_text('main_leader/full_name', node), email:CCB.node_text('main_leader/email', node)},
+                mainleader:{id:CCB.node_attribute('main_leader', 'id', node), name:CCB.node_text('main_leader/full_name', node), email:CCB.node_text('main_leader/email', node)},
                 director:{id:CCB.node_attribute('director', 'id', node), name:CCB.node_text('director/full_name', node), email:CCB.node_text('director/email', node)},
                 modified:{date:CCB.node_text('modified', node), by:{id:CCB.node_attribute('modifier', 'id', node), name:CCB.node_text('modifier', node)}},
                 xml:node.toString()
@@ -114,6 +114,7 @@ exports.database = function database(_CCB) {
             args.modified_since = lastUpdate;
           }
           group.participants = [];
+          group.leaders = [];
           CCB.api("group_participants", function(doc) {
             //console.log(doc.toString());
             _.each(xpath.select('//groups/group/participants/participant', doc), function (node) {
@@ -122,8 +123,14 @@ exports.database = function database(_CCB) {
                 name:CCB.node_text('name', node),
                 status:{id:CCB.node_attribute('status', 'id', node), value:CCB.node_text('status', node)}
               });
+              if (CCB.node_attribute('status', 'id', node) === '1') { //Group Leaders
+                group.leaders.push({id:CCB.node_attribute('.', 'id',node),
+                  name:CCB.node_text('name', node),
+                  status:{id:CCB.node_attribute('status', 'id', node), value:CCB.node_text('status', node)}
+                });
+              }
             });
-            console.log(group);
+            console.log(JSON.stringify(group, null, 2));
             callback();
           }, args);
         }, 1);
